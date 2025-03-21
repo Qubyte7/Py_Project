3d8a226f72d21ef7230784a20f113629cae46a59
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-03-21 15:18:22 +0200

    recommiting after lfs specification

--- machine_learning/backend/yield_predictor/yield_predictor.py
+++ machine_learning/backend/yield_predictor/yield_predictor.py
@@ -2,6 +2,7 @@ import joblib
 import pandas as pd
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 # loading mode ,scaler and frequency mapp
 model = joblib.load("../../models/yield_predictor/crop_yield_model.pkl")
@@ -19,6 +20,13 @@ class YieldPredictorInput(BaseModel):
 
 # initializing the app
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:5173",
+    allow_credentials=True,
+    allow_methods=["GET,POST,PUT,DELETE"],
+    allow_headers=["*"],
+)
 
 
 # defining API
