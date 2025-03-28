b04cce9eab7e03923eedf1e2d4390d63c57f198c
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2025-03-28 21:28:01 +0200

    trying to resolve Cors issues

--- machine_learning/backend/yield_predictor/yield_predictor.py
+++ machine_learning/backend/yield_predictor/yield_predictor.py
@@ -2,7 +2,7 @@ import joblib
 import pandas as pd
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from starlette.middleware.cors import CORSMiddleware
+# from starlette.middleware.cors import CORSMiddleware
 
 # loading mode ,scaler and frequency mapp
 model = joblib.load("../../models/yield_predictor/crop_yield_model.pkl")
@@ -20,13 +20,15 @@ class YieldPredictorInput(BaseModel):
 
 # initializing the app
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins="http://localhost:5173",
-    allow_credentials=True,
-    allow_methods=["GET,POST,PUT,DELETE"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins="http://localhost:5173",
+#     allow_credentials=True,
+#     allow_methods=["GET,POST,PUT,DELETE"],
+#     allow_headers=["*"],
+# )
+#
+
 
 
 # defining API
---
+++ machine_learning/models/yield_predictor/readme.md
@@ -0,0 +1,10 @@
+## Difference betwee .pkl and .pt
+### *.pkl*
+
+Saves the entire Python object (e.g., a scikit-learn pipeline with preprocessing steps and model).
+May include large numpy arrays inefficiently (though joblib is optimized for this).
+
+### *.pt*
+
+Usually stores only the model’s state_dict (weights) by default, not the architecture (unless you save the entire model).
+Architecture must be defined separately when loading (unless saved as a full model).
